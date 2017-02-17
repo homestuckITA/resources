8beaf401c532121f92e8528a4533de6a73b87ee7
Dustine Camacho <dustine.camacho@gmail.com>
2017-02-17 01:36:48 +0000

    Gulp added default task generates the css file based on the sass contents

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const gulp = require('gulp');
+const cleanCSS = require('gulp-clean-css');
+const clean = require('gulp-clean');
+const sass = require('gulp-sass');
+
+gulp.task('default', function(){
+  return gulp.src('sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(cleanCSS({level: 2}))
+    .pipe(gulp.dest('css'));
+});
+
+gulp.task('minify-css', function() {
+    return gulp.src('css/*.css')
+        .pipe(cleanCSS({debug: true}, function(details) {
+            console.log(details.name + ': ' + details.stats.originalSize);
+            console.log(details.name + ': ' + details.stats.minifiedSize);
+        }))
+        .pipe(gulp.dest('css'));
+});
+
+gulp.task('sass', function () {
+ return gulp.src('./scss/**/*.scss')
+   .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+   .pipe(gulp.dest('./css'));
+});
