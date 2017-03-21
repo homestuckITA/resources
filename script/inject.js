42b640e6ddd09eea3640d87ac07784823a5be509
Dustine Camacho <dustine.camacho@gmail.com>
2017-03-21 00:35:23 +0000

    Simplified inject's logic

--- script/inject.js
+++ script/inject.js
@@ -1,17 +1,13 @@
-function inject(selector, classes){
-  if(classes){
-    selector = selector[0] === '#' ? selector : '#' + selector;
-  } else {
-    classes = selector;
-    selector = 'body';
+'use strict'
+
+if(window.inject === 'undefined'){
+  window.inject = function (classes, target) {
+    var $target = $(target || document.currentScript);
+
+    if ($target.is('script')) $target = $target.parent();
+
+    $(function() {
+      $target.addClass(classes);
+    })
   }
-
-  $(function() {
-    $(selector).addClass(classes);
-  })
-}
-
-function injectThis(classes) {
-  console.log(this);
-  $(this).parent().addClass(classes);
 }
