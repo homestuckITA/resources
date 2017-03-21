62a86bb7c4829dbc0b28cc1ef1358a1f345ca191
Dustine Camacho <dustine.camacho@gmail.com>
2017-03-21 00:45:57 +0000

    Debug

--- script/inject.js
+++ script/inject.js
@@ -2,6 +2,8 @@
 
 if(window.inject === undefined){
   window.inject = function (classes, target) {
+    console.log(target);
+    console.log(document.currentScript);
     var $target = $(target || document.currentScript);
 
     if ($target.is('script')) $target = $target.parent();
