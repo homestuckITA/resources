06deec607a70e52973f6a054220560ebd97675d5
Dustine Camacho <dustine@users.noreply.github.com>
2017-04-04 19:28:13 +0100

    Changing class injection to data attributes

--- script/inject.js
+++ script/inject.js
@@ -1,13 +1,34 @@
 'use strict'
 
-if(window.inject === undefined){
-  window.inject = function (classes, target) {
-    var $target = $(target || 'body');
-
-    if ($target.is('script')) $target = $target.parent();
-
-    $(function() {
-      $target.addClass(classes);
+(function(){
+  function inject(data) {
+    data = data | '';
+    console.log('  attribute data: ' + data)
+    console.loh('  starting injection')
+    data.split(' ').map(function(d) {
+      var sep = d.indexOf(':');
+      var target = d.substr(0, sep);
+      var newClass = d.substr(sep + 1);
+      console.log('    injected: ' + target + ':' + newClass);
+      document.getElementById(target).classList.add(newClass);
     })
+    console.log('  injection finished')
   }
-}
+
+  console.log('script run locally')
+  console.log('  #text1 (DOM)');
+  console.log(document.getElementById('text1'));
+  console.log('  #inject (DOM)');
+  console.log(document.getElementById('inject'));
+  var data = document.getElementById('inject').getAttribute('inject');
+  inject(data);
+
+  $(function (){
+    console.log('script run with $()')
+    console.log('  #inject (jQuery)')
+    $inject = $('#inject')
+    console.log($inject)
+    console.log('  #inject data-inject')
+    console.log($inject.data('inject'))
+  })
+})()
