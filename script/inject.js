592c3ec1614845738e9e5a45e509cdc02c59b95a
Dustine Camacho <dustine.camacho@gmail.com>
2017-04-04 21:28:13 +0100

    Default linter didn't work

--- script/inject.js
+++ script/inject.js
@@ -1,32 +1,30 @@
-'use strict'
-
 (function(){
   function inject(data) {
-    data = data | '';
+    data = data | ''
     console.log('  attribute data: ' + data)
     console.loh('  starting injection')
     data.split(' ').map(function(d) {
-      var sep = d.indexOf(':');
-      var target = d.substr(0, sep);
-      var newClass = d.substr(sep + 1);
-      console.log('    injected: ' + target + ':' + newClass);
-      document.getElementById(target).classList.add(newClass);
+      var sep = d.indexOf(':')
+      var target = d.substr(0, sep)
+      var newClass = d.substr(sep + 1)
+      console.log('    injected: ' + target + ':' + newClass)
+      document.getElementById(target).classList.add(newClass)
     })
     console.log('  injection finished')
   }
 
   console.log('script run locally')
-  console.log('  #text1 (DOM)');
-  console.log(document.getElementById('text1'));
-  console.log('  #inject (DOM)');
-  console.log(document.getElementById('inject'));
-  var data = document.getElementById('inject').getAttribute('inject');
-  inject(data);
+  console.log('  #text1 (DOM)')
+  console.log(document.getElementById('text1'))
+  console.log('  #inject (DOM)')
+  console.log(document.getElementById('inject'))
+  var data = document.getElementById('inject').getAttribute('inject')
+  inject(data)
 
   $(function (){
     console.log('script run with $()')
     console.log('  #inject (jQuery)')
-    $inject = $('#inject')
+    var $inject = $('#inject')
     console.log($inject)
     console.log('  #inject data-inject')
     console.log($inject.data('inject'))
